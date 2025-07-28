0b13044b53288be26279385bdd9ba31d6e6ce7d2
Falconlover12347 <falconlover12347@gmail.com>
2025-07-28 21:38:10 +0545

    initial commit

--- authapp/models.py
+++ authapp/models.py
@@ -1,8 +1,7 @@
 # authapp/models.py
-from django.db import models
-from django.contrib.auth.models import User
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class QuizQuestion(models.Model):
     question_text = models.CharField(max_length=255)
@@ -12,17 +11,14 @@ class QuizQuestion(models.Model):
     option4 = models.CharField(max_length=100)
     correct_option = models.IntegerField()  # 1, 2, 3, or 4
 
-def __str__(self):
+    def __str__(self):
         return self.question_text
 
-
-def __str__(self):
-        return self.question_text
 
 class Leaderboard(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
+    def __str__(self):
         return f"{self.user.username} - {self.score}"
--- authapp/views.py
+++ authapp/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import QuizQuestion
+from .models import QuizQuestion, Leaderboard
 from django.http import Http404
 
 def home(request):
-    return render(request, 'Home.html')
+    return render(request, 'home.html')
 
 def login_view(request):
     if request.method == 'POST':
@@ -36,39 +36,10 @@ def register_view(request):
 
 @login_required
 def leaderboard(request):
-    return render(request, 'leaderboard.html')
+    top_scores = Leaderboard.objects.order_by('-score', 'date')[:30]
+    return render(request, 'leaderboard.html', {'top_scores': top_scores})
 
 @login_required
-def quiz_question(request, question_id):
-    try:
-        question = QuizQuestion.objects.get(pk=question_id)
-    except QuizQuestion.DoesNotExist:
-        raise Http404("Question does not exist.")
-
-    if request.method == 'POST':
-        selected_option = request.POST.get('option')
-        if selected_option == question.correct_option:
-            request.session['streak'] = request.session.get('streak', 0) + 1
-        else:
-            return redirect('quiz_result')
-        
-        next_question_id = question_id + 1
-        if QuizQuestion.objects.filter(pk=next_question_id).exists():
-            return redirect('quiz_question', question_id=next_question_id)
-        else:
-            return redirect('quiz_result')
-
-    return render(request, 'quiz_question.html', {'question': question})
-
-@login_required
-def quiz_result(request):
-    score = request.session.get('streak', 0)
-    request.session['streak'] = 0  # reset streak for next time
-    return render(request, 'quiz_result.html', {'score': score})
-
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import QuizQuestion
-
 def quiz_question(request, question_id):
     question = get_object_or_404(QuizQuestion, id=question_id)
 
@@ -83,20 +54,29 @@ def quiz_question(request, question_id):
                 'error': 'Please select an answer.'
             })
 
-        if int(selected) == question.correct:
+        if int(selected) == question.correct_option:
             request.session['score'] += 1
-
             next_question = QuizQuestion.objects.filter(id__gt=question_id).order_by('id').first()
             if next_question:
                 return redirect('quiz_question', question_id=next_question.id)
             else:
+                # No more questions, save score and show result
                 score = request.session.get('score', 0)
+                Leaderboard.objects.create(user=request.user, score=score)
                 request.session.flush()
                 return render(request, 'quiz_result.html', {'score': score})
         else:
+            # Wrong answer, save score and show result
             score = request.session.get('score', 0)
+            Leaderboard.objects.create(user=request.user, score=score)
             request.session.flush()
             return render(request, 'quiz_result.html', {'score': score})
 
     return render(request, 'quiz_question.html', {'question': question})
 
+@login_required
+def quiz_result(request):
+    # This view is kept in case you want to redirect here explicitly
+    score = request.session.get('score', 0)
+    request.session.flush()
+    return render(request, 'quiz_result.html', {'score': score})
